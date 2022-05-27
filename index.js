ab3a1eccd717251554cebf0a4d128e48141d42a4
My Name <ben.d.maryland@gmail.com>
2022-05-27 15:48:29 -0400

    Added sort/edit/delete

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-
-//3. instead of "loading" something nice while loading
-
-
-
-
-
 let allAlbums
 let searchBar = document.querySelector("#header___search-bar")
 let AllAlbumsContainer = document.querySelector("#main__album-container")
 let resultsHeader = document.getElementById("header__results")
 let searchResults
 
-// document.querySelector("#sorter").addEventListener("click", sortHandler)
-// document.querySelector("#filter").addEventListener("click", filterHandler)
-///let url = https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=200
-
-
 document.querySelector("#header___search-btn").addEventListener("click", searchHandler)
 document.querySelector("#header___search-bar").addEventListener("keyup", (e) => { e.key === "Enter" && searchHandler() })
+document.querySelector("#header___sort-btn").addEventListener("click", sortHandler)
+
+
 
 function searchHandler() {
-
     if (searchBar.value === "") {
         alert("Please enter a valid search")
-        console.log("break")
     }
     else {
         resultsHeader.innerText = "◴"
@@ -37,11 +26,9 @@ function searchHandler() {
 function displayAlbums() {
     console.log(allAlbums, searchResults)
     AllAlbumsContainer.textContent = ""
-
     allAlbums.resultCount == 0 ?
         resultsHeader.innerText = `No results results for ${searchBar.value}, please try again` :
         resultsHeader.innerText = `${allAlbums.resultCount} results for ${searchBar.value}`
-
     allAlbums.results.forEach(album => {
 
         // create html items 
@@ -56,33 +43,76 @@ function displayAlbums() {
         albumCard.appendChild(albumImg).src = album.artworkUrl60
         albumCard.className = "card"
 
+        // Deleting
+        let deleteBtn = albumCard.appendChild(document.createElement("button"))
+        deleteBtn.addEventListener("click", deleteHandler)
+        deleteBtn.innerText = "delete"
+        deleteBtn.id = album.collectionId
+
+        let editBtn = albumCard.appendChild(document.createElement("button"))
+        editBtn.addEventListener("click", EditHandler)
+        editBtn.innerText = "edit"
+        editBtn.id = album.collectionId
     });
 
+
 }
 
 
-
-    //◴ ◷ ◶ ◵
-
-let example = {
-    "wrapperType": "collection",
-    "collectionType": "Album",
-    "artistId": 15885,
-    "collectionId": 529020307,
-    "amgArtistId": 44890,
-    "artistName": "Usher",
-    "collectionName": "Versus",
-    "collectionCensoredName": "Versus",
-    "artistViewUrl": "https://music.apple.com/us/artist/usher/15885?uo=4",
-    "collectionViewUrl": "https://music.apple.com/us/album/versus/529020307?uo=4",
-    "artworkUrl60": "https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/ec/d4/ff/ecd4ff16-1cbe-7d2c-df3f-cca0802943e8/884977695137.jpg/60x60bb.jpg",
-    "artworkUrl100": "https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/ec/d4/ff/ecd4ff16-1cbe-7d2c-df3f-cca0802943e8/884977695137.jpg/100x100bb.jpg",
-    "collectionPrice": 7.99,
-    "collectionExplicitness": "notExplicit",
-    "trackCount": 9,
-    "copyright": "℗ 2010 LaFace Records, a unit of Sony Music Entertainment",
-    "country": "USA",
-    "currency": "USD",
-    "releaseDate": "2010-09-22T07:00:00Z",
-    "primaryGenreName": "R&B/Soul"
+function sortHandler(e) {
+    console.log(allAlbums)
+    AllAlbumsContainer.innerHTML = ""
+    allAlbums.results.sort((a, b) => (parseInt(a.releaseDate) - parseInt(b.releaseDate)))
+    displayAlbums()
 }
+
+function deleteHandler(e) {
+    e.target.parentNode.remove()
+
+    // Below is an example of what I'd do to actually delete the item 
+
+    // console.log(e, this.id)
+    // fetch(`https://some_URL/${this.id}`, {
+    //     method: 'DELETE',
+    // }
+    // ).then(data => console.log(data))
+    //     .then(allTodos = allTodos.filter(todo => todo.id != this.id))
+}
+
+
+function EditHandler(e) {
+    // here we replace the H3 title with an input box 
+
+    let newInput = document.createElement('input')
+    let oldTitle = this.parentNode.firstChild
+    let parent = this.parentNode
+    parent.prepend(newInput)
+    newInput.placeholder = oldTitle.innerText
+    oldTitle.remove()
+
+    // here we check for the user to enter a new example and then  once done we replace the old title with the new one 
+    newInput.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            let newTitle = document.createElement('h3')
+            this.parentNode.prepend(newTitle)
+            newTitle.innerText = e.target.value
+            newInput.remove()
+
+            // example fetch request
+
+            // fetch(`https://some_URL/${this.id}`, {
+            //     method: 'PATCH',
+            //     body: JSON.stringify({
+            //         collectionName: e.target.value
+            //     }),
+            //     headers: {
+            //         'Content-type': 'application/json; charset=UTF-8',
+            //     },
+            // })
+            //     .then((response) => response.json())
+            //     .then((json) => console.log(json));
+        }
+    })
+}
+
+
